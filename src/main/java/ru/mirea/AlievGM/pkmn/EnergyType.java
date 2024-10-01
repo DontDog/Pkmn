ee3a8a2fc91edb2af8a18ba26471aa7570e03458
Gleb <87229430+DontDog@users.noreply.github.com>
2024-10-01 14:54:11 +0300

    Initial commit

---
+++ src/main/java/ru/mirea/AlievGM/pkmn/EnergyType.java
@@ -0,0 +1,28 @@
+package ru.mirea.AlievGM.pkmn;
+
+public enum EnergyType {
+    FIRE("FIRE"),
+    GRASS("GRASS"),
+    WATER("WATER"),
+    LIGHTNING("LIGHTNING"),
+    PSYCHIC("PSYCHIC"),
+    FIGHTING("FIGHTING"),
+    DARKNESS("DARKNESS"),
+    METAL("METAL"),
+    FAIRY("FAIRY"),
+    DRAGON("DRAGON"),
+    COLORLESS("COLORLESS");
+
+    private String type;
+
+    EnergyType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return type;
+    }
+}
+
+
---
+++ src/main/java/ru/mirea/AlievGM/pkmn/Student.java
@@ -0,0 +1,57 @@
+package ru.mirea.AlievGM.pkmn;
+
+import java.io.Serializable;
+
+public class Student  implements Serializable {
+    private String firstName;
+    private String surName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " / " + surName + " / " + familyName + " / " + group;
+    }
+
+    private static final long serialVersionUID = 1L;
+    private String familyName;
+    private String group;
+
+    public Student(String firstName, String surName, String familyName, String group) {
+        this.firstName = firstName;
+        this.surName = surName;
+        this.familyName = familyName;
+        this.group = group;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+}
+
